b604e34a23bbc621f2fdec5787d1e97e17b0e7fc
Kadin Weldon <104170431+vontoobskka@users.noreply.github.com>
2024-04-18 10:52:39 -0500

    Add files via upload
    Added sound class and code for it

--- speedstars/src/speedstars/sound.java
+++ speedstars/src/speedstars/sound.java
@@ -1,5 +1,16 @@
-package speedstars;
+import javax.sound.sampled.*;
+import java.io.*;
 
 public class sound {
-
+    public void playmusic(String filepath) {
+        try {
+            File soundFile = new File(filepath);
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioIn);
+            clip.start();
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
 }
