faad53bf60317596fde49a15c60eb5ecd0c2c1f7
Kadin Weldon <104170431+vontoobskka@users.noreply.github.com>
2024-05-06 10:40:41 -0500

    Update Player.java

--- speedstars/Player.java
+++ speedstars/Player.java
@@ -1,23 +1,70 @@
 package speedstars;
 
-import java.awt.image.BufferedImage;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
 
 public class Player {
-    private BufferedImage image;
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private Color color;
 
-    public Player(BufferedImage image) {
-        this.image = image;
+    public Player(int x, int y, int width, int height, Color color) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
     }
 
-    public BufferedImage getImage() {
-        return image;
+    public void draw(Graphics g) {
+        g.setColor(color);
+        g.fillRect(x, y, width, height);
+    }
+
+    public Rectangle getBounds() {
+        return new Rectangle(x, y, width, height);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
     }
 
     public int getWidth() {
-        return image.getWidth();
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
     }
 
     public int getHeight() {
-        return image.getHeight();
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
     }
 }
